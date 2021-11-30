612e927fcb3b5b4a86b843d7dd05587ae073c796
Tobias Pickard <Tobias.Pickard-CIC-UK@ibm.com>
2021-11-30 16:31:18 +0000

    Updating library and docs. Time:2021-11-30 16:31:16.119143, Version:3.9

--- test.py
+++ test.py
@@ -1,20 +1,6 @@
 import os 
 import json
+from datetime import datetime
 
-if os.path.exists('metadata.json'):
-    file = open('metadata.json')
-    data = json.load(file)
-    file.close()
-    
-    with open('metadata.json','w') as file:
-        data['version'] += 0.1
-        data['version'] = round(data['version'],1)
-        file.write(json.dumps(data,indent=4))
-else:
-    with open('metadata.json','w') as file:
-        version = {"version": 0.1}
-        file.write(json.dumps(version, indent=4))
-
-# with open('metadata.json') as file:
-#     data = json.load(file)
-#     print(data)
+now = datetime.now()
+print(now)
