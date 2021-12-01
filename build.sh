75ee98e2a9ababab8c6490e11ad42b24e1c4b4fd
Tobias Pickard <Tobias.Pickard-CIC-UK@ibm.com>
2021-12-01 16:14:47 +0000

    Updating library and docs. Time:2021-12-01 16:14:44.201371, Version:8.4

--- build.sh
+++ build.sh
@@ -9,7 +9,7 @@ nbdev_build_docs
 
 git add . -A
 
-git commit -m "Updating library and docs. Time:2021-12-01 16:14:01.255074, Version:8.3"
+git commit -m "Updating library and docs. Time:2021-12-01 16:14:44.201371, Version:8.4"
 git push
 
 echo Process done
